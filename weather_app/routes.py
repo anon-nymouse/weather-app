bfe4e3645fb8f83352eb34481729882f0e5b3da7
anon-nymouse <77492758+anon-nymouse@users.noreply.github.com>
2021-08-08 20:19:16 +0600

    Update routes.py

--- weather_app/routes.py
+++ weather_app/routes.py
@@ -2,7 +2,8 @@ import requests
 from . import app
 from flask import render_template
 from flask import request
-from weather_app.scripts.get_data import get_location, get_data, get_wdata
+from weather_app.scripts.get_data import get_location, get_data
+from weather_app.scripts.get_wdata import get_wdata
 
 
 api_key = 'c46121cc1ced83e6bc7281bcb5592ca0'

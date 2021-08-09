0e1901cf73b4f385e6772c8b68ed8006d9f96805
anon-nymouse <77492758+anon-nymouse@users.noreply.github.com>
2021-08-09 13:30:02 +0600

    Update routes.py

--- weather_app/routes.py
+++ weather_app/routes.py
@@ -3,18 +3,17 @@ from . import app
 from flask import render_template
 from flask import request
 from weather_app.scripts.get_data import get_location, get_data
-from weather_app.scripts.get_wdata import get_wdata
 
 api_key = 'c46121cc1ced83e6bc7281bcb5592ca0'
 
 @app.route('/')
 def index():
-    city = get_location()
-    data = get_data(city)
-    return render_template('index.html', data=data)
+    #city = get_location()
+    #data = get_data(city)
+    return render_template('index.html')
 
 @app.route('/search', methods=['GET','POST'])
 def search():
     city = request.args.get('city')
-    data = get_wdata(city)
+    data = get_data(city)
     return data

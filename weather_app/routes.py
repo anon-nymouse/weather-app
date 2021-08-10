483a2f6d9dbb5f5e58cf7972f73f469b93d30343
anon-nymouse <anon-nymouse@users.noreply.github.com>
2021-08-10 10:28:27 +0600

    update

--- weather_app/routes.py
+++ weather_app/routes.py
@@ -1,12 +1,15 @@
+import re
+from flask.helpers import url_for
 import requests
 from . import app
 from flask import render_template
-from flask import request
+from flask import request, redirect, url_for
 from weather_app.scripts.get_data import get_location, get_data
 
 api_key = 'c46121cc1ced83e6bc7281bcb5592ca0'
 
 @app.route('/')
+@app.route('/index')
 def index():
     #city = get_location()
     #data = get_data(city)
@@ -14,9 +17,14 @@ def index():
 
 @app.route('/search', methods=['GET','POST'])
 def search():
-    city = request.args.get('city')
-    data = get_data(city)
-    # api_key = 'c46121cc1ced83e6bc7281bcb5592ca0'
-    # url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric'
-    # data = requests.get(url).json()
-    return render_template('results.html', data=data, city=city)
+    if request.method == 'POST':
+        city = request.form.get('city')
+        data = get_data(city)
+        return render_template('results.html', city=city, data=data)
+    return redirect(url_for('index'))
+
+@app.route('/get_weather', methods=['GET', 'POST'])
+def get_weather():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    return render_template('res.html', lat=lat, lon=lon)
